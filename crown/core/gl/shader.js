85a3f79be1180c6d9ed942e5d55cf3c060365ed2
kevin <kevincantstop@gmail.com>
2022-10-04 20:51:18 +0800

    triagle

--- crown/core/gl/shader.js
+++ crown/core/gl/shader.js
@@ -12,10 +12,15 @@ class Shader {
         this.vShader = this._loadShader(vSrc, gl.VERTEX_SHADER)
         this.fShader = this._loadShader(fSrc, gl.FRAGMENT_SHADER)
 
-        this.program = this._create(this.vShader, this.fShader)
+        this.program = this._createProgram(this.vShader, this.fShader)
     }
 
     get name() { return this._name }
+
+    use() {
+        const gl = GlUtils.gl
+        gl.useProgram(this.program)
+    }
 
     _loadShader(src, shaderType) {
         const gl = GlUtils.gl
@@ -25,15 +30,27 @@ class Shader {
         gl.compileShader(shader)
 
         const error = gl.getShaderInfoLog(shader)
-        if (Utils.isEmpty(error)) {
-            throw new Error(`Error compiling shader ${this.name}: ${error}`)
+        if (!Utils.isEmpty(error)) {
+            throw new Error(`Error compiling shader "${this.name}": ${error}`)
         }
 
         return shader
     }
 
-    _create(vShader, fShader) {
+    _createProgram(vShader, fShader) {
+        const gl = GlUtils.gl
+        const program = gl.createProgram()
 
+        gl.attachShader(program, vShader)
+        gl.attachShader(program, fShader)
+
+        gl.linkProgram(program)
+
+        const error = gl.getProgramInfoLog(program)
+        if (!Utils.isEmpty(error)) {
+            throw new Error(`Error link program "${this.name}": ${error}`)
+        }
+        return program
     }
 }
 
--- crown/core/index.js
+++ crown/core/index.js
@@ -19,6 +19,8 @@ class Engine {
         this._loadShaders()
         this._createBuffer()
 
+        this.shader.use()
+
         this._loop()
     }
 
@@ -34,18 +36,34 @@ class Engine {
     _loop() {
         const { gl } = this
 
+        gl.viewport(0, 0, this.canvas.width, this.canvas.height)
         gl.clear(gl.COLOR_BUFFER_BIT)
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer)
+
+        gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0)
+        gl.enableVertexAttribArray(0)
+
+        gl.drawArrays(gl.TRIANGLES, 0, 3)
 
         requestAnimationFrame(() => this._loop())
     }
 
     _createBuffer() {
         const { gl } = this
-        const buffer = new Float32Array([
+        const points = new Float32Array([
             0,   0,   0,
             0,   0.5, 0,
             0.5, 0.5, 0
         ])
+
+        const buffer = gl.createBuffer()
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+        gl.bufferData(gl.ARRAY_BUFFER, points, gl.STATIC_DRAW)
+        gl.bindBuffer(gl.ARRAY_BUFFER, null)
+
+        this.buffer = buffer
     }
 
     _loadShaders() {
@@ -63,6 +81,7 @@ class Engine {
                 gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
             }
         `
+        this.shader = new Shader('basic', vShaderSrc, fShaderSrc)
     }
 }
 
